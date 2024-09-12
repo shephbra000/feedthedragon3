e3105ff59ed346a0450324bfa5140b50c4d5b537
shephbra000 <shephbra000@warren.k12.in.us>
2024-09-12 08:18:46 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 400
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Feed The  Dragon")
+
+
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
